e35e4a6303a8a29d7f6d7c0c309db6c09016a0b5
Tharushi <ththarushi3@gmail.com>
2023-08-08 15:58:57 +0530

    gell all students details

--- frontend/src/components/AllStudents.js
+++ frontend/src/components/AllStudents.js
@@ -9,7 +9,8 @@ export default function AllStudents (){
         function getStudents (){
             axios.get("http://localhost:8070/student/" )
             .then((res) => {
-                setStudents(res.data);
+                console.log(res.data);
+                setStudents(res.data.students);
             })
             .catch((err) =>{
                 alert(err.message)
@@ -21,6 +22,24 @@ export default function AllStudents (){
     return(
         <div className="container">
             <h1>All Students</h1>
+            <table class="table">
+                <thead>
+                    <tr>
+                    <th scope="col">Id</th>
+                    <th scope="col">Name</th>
+                    <th scope="col">Age</th>
+                    <th scope="col">Gender</th>
+                    </tr>
+                </thead>
+                    {students.map((student, index) => (
+                        <tr>
+                            <th scope="row">{index+1}</th>
+                            <td>{student.name}</td>
+                            <td>{student.age}</td>
+                            <td>{student.gender}</td>
+                        </tr>
+                    ))}
+            </table>
         </div>
     )
 }
